66f5a201b48f7524af985d9adb5a4a59f0a7ee1d
Richard Shea <rshea@thecubagroup.com>
2025-01-21 15:12:46 +1300

    Specify the customer user model

--- customuser/core/settings.py
+++ customuser/core/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#substituting-a-custom-user-model
+AUTH_USER_MODEL = "users.CustomUser"
+
